4ff6f21f48fa0ff905614e71857e428ac4e9f6f5
Ysatao <ysaline.lefevre@student.hepl.be>
2025-11-05 15:52:03 +0100

    Village signs & movement
    scene creation, movement from village to wall, signs with weird alphabet

---
+++ Whale's Whispers/Assets/Scripts/ToTheWallEnigma.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToTheWallEnigma : MonoBehaviour
+{
+    public void ToWallEnigma()
+    {
+        SceneManager.LoadSceneAsync("Wall enigma");
+    }
+}
